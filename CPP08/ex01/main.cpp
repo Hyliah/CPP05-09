2a0dd0b11025a50267f59ff360cced1042b797e7
Hygie <lhygie@gmail.com>
2026-02-22 22:09:23 +0100

    CPP08 ex01 ok

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -6,7 +6,61 @@
 /*   By: hlichten <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 22:15:33 by hlichten          #+#    #+#             */
-/*   Updated: 2026/02/19 22:15:35 by hlichten         ###   ########.fr       */
+/*   Updated: 2026/02/22 22:09:03 by hlichten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+int main(){
+    std::srand(std::time(NULL));
+    
+    Span span0(3);
+    Span span1(1000);
+    Span span2(200);
+    Span span3(5);
+
+    std::cout << "------ creation of a defectuous one -------" << std::endl;
+    span0.addNumber(1);
+
+    std::cout << "------ creation of a big old vector -------" << std::endl;
+    for (int i = 0; i < 1000; i++){
+        try {span1.addNumber(::rand()); }
+        catch (std::exception &e) {std::cerr << e.what() << std::endl;}
+    }
+
+
+    std::cout << "------ try the try and catch -------" << std::endl;
+    for (int i = 0; i < 201; i++){
+        try {span2.addNumber(::rand()); }
+        catch (std::exception &e) {std::cerr << e.what() << std::endl;}
+    }
+
+   std::cout << "\n------ small an easy to test -------" << std::endl;
+
+    span3.addNumber(2);
+    span3.addNumber(12);
+    span3.addNumber(29);
+    span3.addNumber(11);
+    span3.addNumber(3);
+
+    std::cout << "------ test the longest and shortest span -------" << std::endl;
+    try
+    {
+        std::cout << "\n -- shortest : 3 - 2 = 1 // longest : 29 - 12 = 17" << std::endl;
+        std::cout << "Span3 shortest: " << span3.shortestSpan() << std::endl;
+        std::cout << "Span3 longest : " << span3.longestSpan() << std::endl;
+        std::cout << "\n -- test on the big one with random numbers" << std::endl;
+        std::cout << "Span1 shortest: " << span1.shortestSpan() << std::endl;
+        std::cout << "Span1 longest : " << span1.longestSpan() << std::endl;
+        std::cout << "\n -- defectuous one" << std::endl;
+        std::cout << "Span0 longest : " << span0.longestSpan() << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
--- CPP08/ex01/src/Span.cpp
+++ CPP08/ex01/src/Span.cpp
@@ -6,29 +6,59 @@
 /*   By: hlichten <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 22:45:51 by hlichten          #+#    #+#             */
-/*   Updated: 2026/02/20 02:12:59 by hlichten         ###   ########.fr       */
+/*   Updated: 2026/02/22 21:50:24 by hlichten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+#include <algorithm>
 
 Span::Span(unsigned int N) : _maxSize(N){}
-Span::Span(const Span &other) : _maxSize(other._maxSize), _numbers(other._numbers){}
+Span::Span(const Span &other) : _numbers(other._numbers), _maxSize(other._maxSize){}
 Span& Span::operator=(const Span &other){
-    if (this != other){
+    if (this != &other){
+        _numbers = other._numbers;
         _maxSize = other._maxSize;
-        _numbers = other._numbers
-    return (*this);
     }
+    return (*this);
 }
 Span::~Span(){}
 
-void Span::addNumber(int N){
+void Span::addNumber(int number)
+{
+    if (_numbers.size() >= _maxSize)
+        throw std::runtime_error("Span is full");
 
+    _numbers.push_back(number);
 }
 unsigned int Span::shortestSpan() const {
+    if (_numbers.size() < 2)
+        throw std::runtime_error("Min 2 elements needed to find span");
+    
+    std::vector<int> tmp = _numbers;
+    std::sort(tmp.begin(), tmp.end());
 
+    unsigned int shortest = tmp[1] - tmp[0];
+    for (size_t i = 1; i < tmp.size(); i++){
+        unsigned int diff = (tmp[i] - tmp[i - 1]);
+        if (diff < shortest)
+            shortest = diff;
+    }
+    return shortest;
 }
 unsigned int Span::longestSpan() const {
+    if (_numbers.size() < 2)
+        throw std::runtime_error("Min 2 elements needed to find span");
+    
+    std::vector<int> tmp = _numbers;
+    std::sort(tmp.begin(), tmp.end());
 
+    unsigned int longest = tmp[1] - tmp[0];
+    for (size_t i = 1; i < tmp.size(); i++){
+        unsigned int diff = (tmp[i] - tmp[i - 1]);
+        if ( diff > longest)
+            longest = diff;
+    }
+    return longest;
 }
+
